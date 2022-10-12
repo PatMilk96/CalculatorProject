617c490878da127d9ce7447d12db717181fc7d8f
Patryk Milkiewicz <g00397265@atu.ie>
2022-10-12 13:23:21 +0100

    Added an add class

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        add();
+    }
 
+    public static void add(){
+        System.out.printf("Enter First Number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNum = inputs.nextInt();
+
+        System.out.printf("Enter Second Number: ");
+        int secondNum = inputs.nextInt();
+
+        int total = firstNum + secondNum;
+        System.out.printf("The total is: " + total);
     }
 }
