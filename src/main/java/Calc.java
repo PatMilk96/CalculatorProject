dedde39afc81da41e18279cc50a3a7eef64922e5
g00379597 <g00379597@atu.ie>
2022-10-12 13:08:18 +0100

    Added Feature_sub Function

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+             sub();
+    }
 
+    public static void sub()
+    {
+        System.out.println("Please your first number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please your Second number");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber - secondNumber;
+        System.out.println("Total is "+ total);
     }
 }
