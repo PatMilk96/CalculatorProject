58cb98d81c40c68455b5e49b27d10dff06b9133e
g00379597 <g00379597@atu.ie>
2022-10-12 13:32:14 +0100

    Added Multiply Function

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -2,20 +2,31 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
-
         add();
+        multiply();
+    }
+    public static void multiply() {
+        System.out.println("Your first number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Your Second number");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("Total is "+ total);
     }
 
     public static void add(){
-        System.out.printf("Enter First Number: ");
+        System.out.println("Enter First Number: ");
         Scanner inputs = new Scanner(System.in);
         int firstNum = inputs.nextInt();
 
-        System.out.printf("Enter Second Number: ");
+        System.out.println("Enter Second Number: ");
         int secondNum = inputs.nextInt();
 
         int total = firstNum + secondNum;
-        System.out.printf("The total is: " + total);
+        System.out.println("The total is: " + total);
 
              sub();
     }
